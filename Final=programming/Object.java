8e7a70be2d3553d226bf16422f09544ab9ece06b
NmaaAlhawary <20110037@htu.edu.jo>
2021-06-02 22:33:08 +0300

    Final

---
+++ Final=programming/Object.java
@@ -0,0 +1,19 @@
+package Programming;
+
+public class Object {
+
+	public static void main(String[] args) {
+		
+
+TestObject object=new TestObject(""); 
+object.country="amman";
+System.out.println(object.country);
+object.setAge(10);
+System.out.println(object.getAge());
+object.setName("nama");
+
+		
+	}
+}
+
+
---
+++ Final=programming/TestObject.java
@@ -0,0 +1,40 @@
+package Programming;
+
+public class TestObject{ 
+ private String name; 
+ private int age;
+ String country;
+
+		 public TestObject (String country)
+	             {
+				 		
+
+			    this.country=country;
+				 		
+			              }
+				
+		 public int getAge()
+				{
+				 	return age;	
+				 	     }
+		  public void setAge(int age) {
+			  this.age=age;
+		
+		  }
+				 
+		
+
+		public String getName()
+			{
+			 	return name;	
+			 	     }
+		  
+		  public void setName(String name)
+			{
+			this.name=name;
+			System.out.println(name);
+			 	     }
+}
+		  
+				
+
